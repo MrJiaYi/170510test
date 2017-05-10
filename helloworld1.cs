111d6d77c972c124407d7223f099f029987a9eaf
racoooooooon <jlzhang890629@126.com>
2017-05-10 23:58:46 +0800

    second commit

--- helloworld1.cs
+++ helloworld1.cs
@@ -6,7 +6,14 @@ public class helloworld1 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		print("hello world");
+		//print("hello world");
+		int score = 0;
+		float score2 = 23.0f;
+		bool shifoubiye = true;
+
+		print (score);
+		print (score2);
+		print (shifoubiye);
 	}
 	
 	// Update is called once per frame

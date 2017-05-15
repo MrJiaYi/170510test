86fbb544ab631969df4bd68dc7c203fa1b7d0a00
racoooooooon <jlzhang890629@126.com>
2017-05-15 23:25:38 +0800

    qiantao xunhuan

--- createwall.cs
+++ createwall.cs
@@ -8,7 +8,12 @@ public class createwall : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject.Instantiate (zhuankuai, new Vector3 (0, 0, 0), Quaternion.identity);
+
+		for (int i = 0; i <= 10; i++) {
+			for (int j = 0; j <= 10; j++) {
+				GameObject.Instantiate (zhuankuai, new Vector3 (i, j, 0), Quaternion.identity);
+			}
+		}
 	}
 	
 	// Update is called once per frame

f14bbc9a0a056f2e3140b5e0017a05e09bc4148b
racoooooooon <jlzhang890629@126.com>
2017-05-17 23:51:03 +0800

    continue & break

--- createwall.cs
+++ createwall.cs
@@ -14,14 +14,28 @@ public class createwall : MonoBehaviour {
 //				GameObject.Instantiate (zhuankuai, new Vector3 (i, j, 0), Quaternion.identity);
 //			}
 //		}
-		int numx=0,numy=0;
-		while (numx < 10) {
-			numy = 0;
-			while (numy < 10) {
-				GameObject.Instantiate (zhuankuai, new Vector3 (numx, numy, 0), Quaternion.identity);
-				numy++;
+//		int numx=0,numy=0;
+//		while (numx < 10) {
+//			numy = 0;
+//			while (numy < 10) {
+//				GameObject.Instantiate (zhuankuai, new Vector3 (numx, numy, 0), Quaternion.identity);
+//				numy++;
+//			}
+//			numx++;
+//		}
+
+		for (int i=0;i>=-8;i--){
+			for (int j = 0; j <= 8; j++) {
+				// if (-7 <= i <= -1 && 1 <= j <= 7) wrong grammer cant compare continue
+				if (i <= -1 && i >= -7 && j >= 1 && j <= 7)
+				{
+					continue;
+				}
+				GameObject.Instantiate (zhuankuai, new Vector3 (i, j, 0), Quaternion.identity);
 			}
-			numx++;
+			if (i == -4) {
+				break;
+			}
 		}
 	}
 	
